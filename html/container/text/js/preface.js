7dc58f9e2564d09a88d72b91e6a738b4dbd47b81
Syuusao Kou <Syuusao Kou>
2016-09-17 03:19:19 +0800

    update

--- html/container/text/js/preface.js
+++ html/container/text/js/preface.js
@@ -1,7 +1,6 @@
-var anime_begin_index = 0;
+//load control
 var stalled_time = 0;
 
-var animes = document.getElementsByClassName("init_anime");
 var player = document.getElementById("player");
 var all_unit = document.getElementById("all_unit");
 var loader = document.getElementById("loader");
@@ -15,41 +14,31 @@ function remove_item_when_anime_finish()
 function skip()
 {
 	player.removeEventListener("stalled", all_unit_stop);
-	player/removeEventListener("timeupdate", all_unit_start);
-	player.volume = 0.3;
+	player.removeEventListener("timeupdate", all_unit_start);
+	player.volume = 0.1;
+	loader.style.display = "none";
 	all_unit.style.display = "none";
 	last.style.display = "block";
-	last.style.animationPlayState = "paused";
-	last.style.transition = "opacity 2s ease-out 0s";
-	last.style.opacity = "1.0";
 }
 
 function all_unit_stop()
 {
-	for(var i = anime_begin_index;i < animes.length; i++)
-	{
-		animes.item(i).style.animationPlayState = "paused";
-	}
-	all_unit.style.opacity = "0.3";
+	all_unit.style.opacity = "0.1";
 	loader.style.display = "block";
-	stalled_time = stalled_time + 1;
-	player.addEventListener("timeupdate", all_unit_start);
-}
-
-function all_unit_start()
-{
 	if(stalled_time > 3)
 	{
 		player.pause();
 		alert("We just find your network sucks. Now let us skip this whole preface and get the main dish.")
 		skip();
 	}
+	stalled_time = stalled_time + 1;
+	player.addEventListener("timeupdate", all_unit_start);
+}
+
+function all_unit_start()
+{
 	loader.style.display = "none";
 	all_unit.style.opacity = "1.0";
-	for(var i = anime_begin_index;i < animes.length; i++)
-	{
-		animes.item(i).style.animationPlayState = "running";
-	}
 	player.removeEventListener("timeupdate", all_unit_start);
 }
 
@@ -59,9 +48,65 @@ window.onload = function()
 	player.play();
 }
 
-for(var i = 0;i < animes.length; i++)
-{
-	animes.item(i).addEventListener("animationend", remove_item_when_anime_finish);
-}
 player.addEventListener("timeupdate", all_unit_start);
 player.addEventListener("stalled", all_unit_stop);
+player.addEventListener("ended", function()
+{
+	player.removeEventListener("stalled", all_unit_stop);
+	player.removeEventListener("timeupdate", all_unit_start);
+	loader.style.display = "none";
+	all_unit.style.display = "block";
+});
+
+//follow the mouse move
+function mouse_move(event)
+{
+	w = event.clientX;
+	h = event.clientY;
+
+	if(w < screen_w)
+	{
+		for(var i = 0;i < slider.length; i++)
+		{
+			slider.item(i).style.left = (original_pos_w[i] * 100 + (4 - i)) + "%";
+		}
+	}
+	else
+	{
+		for(var i = 0;i < slider.length; i++)
+		{
+			slider.item(i).style.left = (original_pos_w[i] * 100 - (4 - i)) + "%";
+		}
+	}
+
+	if(h < screen_h)
+	{
+		for(var i = 0;i < slider.length; i++)
+		{
+			slider.item(i).style.top = (original_pos_h[i] * 100 + (4 - i)) + "%";
+		}
+	}
+	else
+	{
+		for(var i = 0;i < slider.length; i++)
+		{
+			slider.item(i).style.top = (original_pos_h[i] * 100 - (4 - i)) + "%";
+		}
+	}
+}
+
+var screen_w = window.innerWidth / 2;
+var screen_h = window.innerHeight / 2;
+var slider = document.getElementsByClassName("slide");
+var original_pos_w = [];
+var original_pos_h = [];
+[].forEach.call(slider, function(a){original_pos_w.push((parseInt(window.getComputedStyle(a).left) / window.innerWidth).toFixed(2));});
+[].forEach.call(slider, function(a){original_pos_h.push((parseInt(window.getComputedStyle(a).top) / window.innerHeight).toFixed(2));});
+var w = 0;
+document.addEventListener("mousemove", mouse_move, false);
+
+window.onresize = function()
+{
+	screen_w = window.innerWidth / 2;
+	screen_h = window.innerHeight / 2;
+}
--- nodejs/viewer.coffee
+++
@@ -1,6 +0,0 @@
-fs = require("fs")
-
-show = (layout, data) ->
-	# to do...
-
-exports.show = show
